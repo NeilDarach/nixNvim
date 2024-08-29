c70df2e476fc7f81e5315952ef88865da1b66b32
Neil Darach <neil.darach@gmail.com>
2024-08-29 22:45:02 +0100

    Autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,12 +1,11 @@
-return {
-	"windwp/nvim-autopairs",
-	enable = require("nixCatsUtils").enableForCategory("kickstart-autopairs"),
-	event = "InsertEnter",
-	dependencies = { "hrsh7th/nvim-cmp" },
-	config = function()
-		require("nvim-autopairs").setup({})
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		local cmp = require("cmp")
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end,
-}
+require("lze").load({
+    "nvim-autopairs",
+    event = "InsertEnter",
+    after = function(plugin)
+        require("nvim-autopairs").setup({})
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+})
+
